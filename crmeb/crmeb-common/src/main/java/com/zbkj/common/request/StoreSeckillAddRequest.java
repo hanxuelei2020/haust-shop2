f2fbdadec378cb43718c6d59fde9bb053e8f0c03
Han Xuelei <2407411399@qq.com>
2023-05-03 04:35:10 +0000

    修改spring boot的版本 增加java的版本 添加@NotBlank注解 添加Validation

--- crmeb/crmeb-common/src/main/java/com/zbkj/common/request/StoreSeckillAddRequest.java
+++ crmeb/crmeb-common/src/main/java/com/zbkj/common/request/StoreSeckillAddRequest.java
@@ -6,10 +6,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
