aa1bf00adf196f4ea139805ef87a2489e8f9ba69
hanxuelei <2407411399@qq.com>
2023-05-03 22:04:27 +0800

    迁移springdoc

--- crmeb/crmeb-front/src/main/java/com/zbkj/front/pub/GetJSConfig.java
+++ crmeb/crmeb-front/src/main/java/com/zbkj/front/pub/GetJSConfig.java
@@ -4,6 +4,8 @@ import com.zbkj.common.constants.Constants;
 import com.zbkj.service.service.SystemConfigService;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
