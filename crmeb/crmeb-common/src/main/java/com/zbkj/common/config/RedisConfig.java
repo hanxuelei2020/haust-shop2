a5a2197ddfbd4cc95098b401b2b098aeb38c66f6
hanxuelei <2407411399@qq.com>
2023-05-03 20:21:26 +0800

    spring-data-redis需要jedis或者lettuce,因为需要一个redisFactory

--- crmeb/crmeb-common/src/main/java/com/zbkj/common/config/RedisConfig.java
+++ crmeb/crmeb-common/src/main/java/com/zbkj/common/config/RedisConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
